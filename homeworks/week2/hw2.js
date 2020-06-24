8cbf64ac32a647f9692d113f2873ed100ae38ee0
Linda <lindatsou108@gmail.com>
2020-06-25 02:09:32 +0800

    week2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,13 @@
-function capitalize(str) {
-  
+function capitalize(str) { 
+    var result = ''
+    var firstLetter = str[0]
+    for (var i = 1; i < str.length; i++) {
+        if (str[0] >= 'a' && str[0] <= 'z') {
+            firstLetter = str[0].toUpperCase()
+        }
+        result += str[i]
+    }
+    return firstLetter + result
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize(',,nick'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,28 @@
 function join(arr, concatStr) {
-  
+    var result = arr[0]
+    for (var i = 1; i < arr.length; i++) {
+        result += concatStr + arr[i]
+    }
+  return result
 }
 
 function repeat(str, times) {
-  
+  var result = ''
+  for (var i = 1; i <= times; i++) {
+      result += str
+  }
+  return result
 }
 
-console.log(join(['a'], '!'));
+console.log(join([1, 2, 3], ''));
 console.log(repeat('a', 5));
+
+/*
+console.log(join([1, 2, 3], ''))  ，正確回傳值：123
+console.log(join(["a", "b", "c"], "!"))  ，正確回傳值：a!b!c
+console.log(join(["a", 1, "b", 2, "c", 3], ','))  ，正確回傳值：a,1,b,2,c,3
+
+
+console.log(repeat('a', 5))//，正確回傳值：aaaaa
+console.log(repeat('yoyo', 0))//正確回傳值：yoyoyoyo
+*/
