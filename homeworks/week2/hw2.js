5932c4a016c785df1e980cf0e879e59df5cdd74a
Linda <lindatsou108@gmail.com>
2020-06-29 22:58:33 +0800

    hw2 corrected

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,13 +1,14 @@
 function capitalize(str) { 
     var result = ''
     var firstLetter = str[0]
+    if (str[0] >= 'a' && str[0] <= 'z') {
+        firstLetter = str[0].toUpperCase()
+    }
+
     for (var i = 1; i < str.length; i++) {
-        if (str[0] >= 'a' && str[0] <= 'z') {
-            firstLetter = str[0].toUpperCase()
-        }
         result += str[i]
     }
     return firstLetter + result
 }
 
-console.log(capitalize(',,nick'));
+console.log(capitalize('nick'));
