a0a1cadc74e1119ca182d9c1672999a32c60a805
Linda <lindatsou108@gmail.com>
2020-08-19 18:17:20 +0800

    week7 finished

--- homeworks/week7/hw2/faq.js
+++ homeworks/week7/hw2/faq.js
@@ -2,6 +2,8 @@ document
   .querySelector('.section')
   .addEventListener('click', (e) => {
     const faqBlock = e.target.closest('.section__question').classList;
-    faqBlock.toggle('hide');
-    faqBlock.toggle('question__btn');
+    if (faqBlock) {
+      faqBlock.toggle('hide');
+      faqBlock.toggle('question__btn');
+    }
   });
